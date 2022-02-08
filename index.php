dcd1828084ec29695f6fb78a8baaa6c3cc51fe12
darkerWorrior <91236979+darkerWorrior@users.noreply.github.com>
2022-02-08 10:29:44 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * ====================================================================================
+ *                           Google Drive Proxy Player (c) CodySeller
+ * ----------------------------------------------------------------------------------
+ * @copyright This software is exclusively sold at codester.com. If you have downloaded this
+ *  from another site or received it from someone else than me, then you are engaged
+ *  in an illegal activity. You must delete this software immediately or buy a proper
+ *  license from https://www.codester.com/codyseller?ref=codyseller.
+ *
+ *  Thank you for your cooperation and don't hesitate to contact me if anything :)
+ * ====================================================================================
+ *
+ * @author CodySeller (http://codyseller.com)
+ * @link http://codyseller.com
+ * @license http://codyseller.com/license
+ */
+
+
+if(!file_exists("includes/config.php")){
+  header("Location: install");
+  exit;
+}
+
+require_once(__DIR__.'/includes/config.php');
+
+$app->run();
